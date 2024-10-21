3dc28f815f2d3b3a6dbd77015e9a80fb58a0c281
agrailard <agrailard@gmail.com>
2024-10-21 12:01:00 +0200

    Séance 2

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,10 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final int QUALITE_MAX = 50;
+    public static final String ITEM_BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String ITEM_BRIE = "Aged Brie";
+    public static final String ITEM_SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,56 +13,68 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (this.isObjetPerdantEnQualité(item) && item.quality > 0) {
-                item.quality = item.quality - 1;
-
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (this.isObjetPerdantEnQualité(item)) {
-                    if (item.quality > 0) {
+            if (!item.name.equals(ITEM_BRIE)
+                    && !item.name.equals(ITEM_BACKSTAGE)) {
+                if (item.quality > 0) {
+                    if (!item.name.equals(ITEM_SULFURAS)) {
                         item.quality = item.quality - 1;
                     }
                 }
-                else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            } else {
+                if (item.quality < QUALITE_MAX) {
+                    gagneEnQualite(item);
+
+                    if (item.name.equals(ITEM_BACKSTAGE)) {
+                        if (item.sellIn <= 10) {
+                            if (item.quality < QUALITE_MAX) {
+                                gagneEnQualite(item);
+                            }
+                        }
+
+                        if (item.sellIn <= 5) {
+                            if (item.quality < QUALITE_MAX) {
+                                gagneEnQualite(item);
+                            }
+                        }
+                    }
+                }
+            }
+
+            decrementeNbJoursAvantPeremption(item);
+
+            if (estPerime(item)) {
+                if (item.name.equals(ITEM_BACKSTAGE)) {
                     item.quality = 0;
                 }
-                else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                if (!item.name.equals(ITEM_BRIE)) {
+                    if (!item.name.equals(ITEM_BACKSTAGE)) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(ITEM_SULFURAS)) {
+                                item.quality = item.quality - 1;
+                            }
+                        }
+                    }
+                } else {
+                    if (item.quality < QUALITE_MAX) {
+                        gagneEnQualite(item);
                     }
                 }
             }
         }
     }
 
-
-
-    private boolean isObjetPerdantEnQualité(Item item){
-        return !item.name.equals("Aged Brie")
-            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
-            && !item.name.equals("Sulfuras, Hand of Ragnaros");
+    private static boolean estPerime(Item item) {
+        return item.sellIn < 0;
     }
+
+    private static void decrementeNbJoursAvantPeremption(Item item) {
+        if (!item.name.equals(ITEM_SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private static void gagneEnQualite(Item item) {
+        item.quality = item.quality + 1;
+    }
+
 }
